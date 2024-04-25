d75975e01791416dfe39605f1f4a693c20330069
Sluur <rodrigo.l.haro@gmail.com>
2024-04-25 01:29:44 -0300

    End of section 5

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Activity } from "../../../app/models/activity";
+import {
+    CardMeta,
+    CardHeader,
+    CardDescription,
+    CardContent,
+    Card,
+    Image,
+    Button,
+  } from 'semantic-ui-react'
+
+
+  interface Props {
+    activity: Activity;
+    cancelSelectActivity: () => void;
+    openForm: (id: string) => void;
+
+}
+
+
+export default function ActivityDetails({activity, cancelSelectActivity, openForm}: Props) {
+    return(
+        <Card fluid>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+            <CardContent>
+                <CardHeader>{activity.title}</CardHeader>
+                <CardMeta>
+                    <span>{activity.date}</span>
+                </CardMeta>
+                <CardDescription>
+                    {activity.description}
+                </CardDescription>
+            </CardContent>
+            <CardContent>
+                <Button.Group widths='2'>
+                    <Button onClick={() => openForm(activity.id)} basic color="blue" content='Edit'/>
+                    <Button onClick={cancelSelectActivity} basic color="grey" content='Cancel'/>
+                </Button.Group>
+            </CardContent>
+        </Card>
+    )
+
+}
